6cee0deb6e5d31f940da9001bb508900727130a1
Turkinass <66798896+Turkinass@users.noreply.github.com>
2020-11-18 15:54:28 +0300

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,2 @@
+201745090 - Section 01 - Turki Alsaedi
+Website URL : https://turkinass.github.io/SWE363-HW3/index.html
---
+++ script.js
@@ -0,0 +1,7 @@
+function search(){
+    document.getElementById("search").style.transform ="none";
+}
+
+function navmenu(){
+    document.getElementById("navmenu").style.transform ="none";
+}
